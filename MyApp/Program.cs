8ed3a2d66c2b84f9fe5dbcaa37cade632d12a4ac
congdo20 <nguyencongdo20@gmail.com>
2025-11-10 17:16:52 +0700

    add new chsarp project

---
+++ MyApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
